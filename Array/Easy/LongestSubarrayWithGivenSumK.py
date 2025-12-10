b260729ab453ad8a841760ee374fa6cf35c9e8c9
krishnalad17 <krishna.18152@sakec.ac.in>
2025-12-10 16:13:00 +0530

    first commit

--- Array/Easy/LongestSubarrayWithGivenSumK.py
+++ Array/Easy/LongestSubarrayWithGivenSumK.py
@@ -4,9 +4,8 @@ def longest(n,k):
         curr_sum=0
         for j in range(i,len(n)):
             curr_sum+=n[j]
-            j+=1
             if curr_sum==k:
-                maxlength=max(maxlength,j-i)
+                maxlength=max(maxlength,j-i+1)
     return maxlength
             
 print(longest(n=[1,1,1,4,1,1,1,2],k=5))
